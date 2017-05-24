d9c85cd424e6dd3493eccf0df154ded1e1975084
solarthief <solarthief@users.noreply.github.com>
2017-05-24 21:12:28 +0300

    fixed ascii file;

--- lex.h
+++ lex.h
@@ -4,9 +4,10 @@
 
 struct token {
 	unsigned int code;
+	string ll;
 	unsigned int row_pos;
 	unsigned int pos;
-	token(unsigned int c, unsigned int r, unsigned int p) :code(c), row_pos(r), pos(p) {};
+	token(unsigned int c, unsigned int r, unsigned int p, string s) :code(c), row_pos(r), pos(p), ll(s) {};
 };
 
 struct err {
@@ -24,7 +25,7 @@ public:
 	tbl() :lexems() {};
 	tbl(const tbl &l) :lexems(l.lexems) {};
 	void lex_analys(string file_path);
-	void push(unsigned int, unsigned int, unsigned int);
+	void push(unsigned int, unsigned int, unsigned int,string s);
 	void er_push(string, unsigned int, unsigned int);
 	vector<token> get_lexems();
 	vector<err> get_errors();
